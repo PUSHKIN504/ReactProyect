dbfe76f76be674f95bbea5f7db8e58321c791b51
Madian2112 <161340614+Enderson9@users.noreply.github.com>
2024-05-28 12:16:52 -0600

    fdg
    gfd

--- src/components/layout-components/NavPanel.js
+++ src/components/layout-components/NavPanel.js
@@ -17,7 +17,22 @@ export const NavPanel = ({ direction, mode }) => {
 		setOpen(false);
 	};
 	
-	return
+	return (
+		<>
+			<NavItem onClick={showDrawer} mode={mode}>
+				<SettingOutlined className="nav-icon mr-0" />
+			</NavItem>
+			<Drawer
+				title="Theme Config"
+				placement={direction === DIR_RTL ? 'left' : 'right'} 
+				width={350}
+				onClose={onClose}
+				open={open}
+			>
+				<ThemeConfigurator/>
+			</Drawer>
+		</>
+	)
 }
 
 export default NavPanel;
